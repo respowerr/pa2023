8f8fa1993ddd8e2970f8c9e2c0e26cc6c997d59d
Respowerr <50842024+respowerr@users.noreply.github.com>
2024-04-30 14:30:51 +0200

    Delete

--- secrets.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is your test secret API key.
-$stripeSecretKey = 'sk_test_51N4siGE2uBhK8kS7vxxI2yP6k4TByk75zM2TV5aGj7fEFKUMLsrwmlVaeyLP4Mdx0Gcw9FFevpoJnkrA5Q5aFlt300muk8ZJIs';
-?>
